b3aa076cb89e95632dd7257c796ab755ce06b520
bhankymyk <bankole123michael@gmail.com>
2022-09-16 16:27:59 +0100

    README.md n 0x0A

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create a file in /tmp
+
+file {'/tmp/school':
+path => '/tmp/school':
+permission => '0744':
+owner => 'www-data':
+group => 'www-data':
+contains => 'I love puppet'
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+This readme for 0x0A-configuration_management
