76c1ae2e08378b2f87e54b05a2a6dfb89a7098d1
bhankymyk <bankole123michael@gmail.com>
2022-09-17 16:12:33 +0100

    0-create_a_file.pp modified again

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # create a file in /tmp
 file {'/tmp/school':
     path        => '/tmp/school',
-    permission  => '0744',
+    mode  => '0744',
     owner       => 'www-data',
     group       => 'www-data',
     content    => 'I love Puppet',
