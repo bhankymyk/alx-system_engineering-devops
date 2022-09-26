8b47a3b559f6a260959f9f863a2335dea811019d
bhankymyk <bankole123michael@gmail.com>
2022-09-26 15:39:36 +0100

    100-puppet_ssh_config.pp modified again

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,7 +3,7 @@ include stdlib
 
 file_line { 'Turn off passwd auth':
   ensure  => 'present',
-  path    => 'etc/ssh/ssh_config',
+  path    => '/etc/ssh/ssh_config',
   line    => '  PasswordAuthentication no',
   replace => true,
 }
