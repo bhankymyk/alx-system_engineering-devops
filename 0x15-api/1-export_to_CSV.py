4b8819efb6f1b7d20e883b536508837a099ee865
bhankymyk <bankole123michael@gmail.com>
2022-10-17 12:03:50 +0100

    convert to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This script to export task done in task #0 to CSV format
+"""
+
+import csv
+import requests
+from sys import orig_argv
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(argv[1])).json()
+    todo_list = requests.get(url + "todos", params={"userId": argv[1]}).json()
+    USER_ID = argv[1]
+    USERNAME = user.get('username')
+
+    with open("{}.csv".format(USER_ID), 'w', newline='') as csvfile:
+        user_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todo_list:
+            user_writer.writerow([int(USER_ID), user.get('username'),
+                                 task.get('completed'),
+                                 task.get('title')])
